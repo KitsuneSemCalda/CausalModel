a02a82575ad84180ae94ff3fe7c14b3be1aacf85
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-16 09:52:07 -0300

    refactor(note): comment is a note about the enum

--- Include/CausalModel/KnowledgeForm.hpp
+++ Include/CausalModel/KnowledgeForm.hpp
@@ -2,7 +2,7 @@
 
 #include <cstdint>
 
-// Formal  declaration status of the knowledge.
+// NOTE: Formal  declaration status of the knowledge.
 enum class KnowledgeForm : uint8_t {
   Axiom,     ///< Accepted as a foundational truth
   Dogma,     ///< Accepted by authority or tradition
